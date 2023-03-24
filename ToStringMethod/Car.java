a8465a1de78f50757dc2c3d60b651b2ec46c6b28
Pau Siquier <pausiqrey8@gmail.com>
2023-03-24 15:05:51 +0100

    toString method

---
+++ ToStringMethod/Car.java
@@ -0,0 +1,23 @@
+
+public class Car {
+
+	//variables
+	String make = "ford";
+	String model = "mustang";
+	String color = "red";
+	int year = 2021;
+	
+	
+	/********/
+	
+	public String toString(){//no void, string because will return a String
+		
+		//String myString = make + "\n"+model+"\n"+color+"\n"+year;
+		//return myString;
+	
+		return make + "\n"+model+"\n"+color+"\n"+year; //same but 1 line, will return all the attributes of our car
+		//that returns a string that "textually represents" an object. ==> ford mustang red 2021
+		
+		
+	}
+}
---
+++ ToStringMethod/toString.java
@@ -0,0 +1,33 @@
+// toString() = special method that all objects inherit
+//				that returns a string that "textually represents" an object.
+//				can be used both implicitly and explicitly
+
+
+
+
+public class toString {
+	
+	public static void main (String[] args) {
+		
+		Car car = new Car();
+		
+		System.out.println(car.model); //display attribute model
+		System.out.println(car.make);
+		System.out.println(car.color);
+		System.out.println(car.year);
+		
+		System.out.println(car.toString()); // ==> Car@4517d9a3  == Address of Car in the memories !!ThE SaMe!!
+		System.out.println(car); // ==> Car@4517d9a3  == Address of Car in the memories
+		
+		/*******/
+		System.out.println(car.toString()); // ==> mustang ford red 2021
+		System.out.println(car); // ==> mustang ford red 2021
+	
+		
+		}
+	
+	}
+
+
+
+

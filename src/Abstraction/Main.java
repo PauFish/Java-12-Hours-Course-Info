57180aada6e7e4b53bf504d7d02480c275df2c2e
Pau Siquier <pausiqrey8@gmail.com>
2023-03-27 15:32:09 +0200

    Abstract

---
+++ src/Abstraction/Main.java
@@ -0,0 +1,21 @@
+//  Abstract = abstract classes cannot be instantiated, but can have a subclass.
+//		    	abstract methods are declared without an implementation.	
+package Abstraction;
+
+public class Main {
+	
+	public static void main (String[] args) {
+		
+		
+		//Vehicle vehicle = new Vehicle(); it's not possible, canot create a vehicle
+		Car car = new Car();
+		
+		
+	
+		
+		}
+	}
+
+
+
+
---
+++ src/Abstraction/Vehicle.java
@@ -0,0 +1,8 @@
+ package Abstraction;
+
+public abstract class Vehicle {
+
+	abstract void go();//void=return something
+	//we are obligated to be override in all children classes
+	
+}

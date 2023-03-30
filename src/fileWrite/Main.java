d8df14f5282ee90d9da6c5beec448d7f32f188ae
Pau Siquier <pausiqrey8@gmail.com>
2023-03-30 17:25:35 +0200

    file writers

---
+++ src/fileWrite/Main.java
@@ -0,0 +1,21 @@
+package fileWrite;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	try {
+		FileWriter writer = new FileWriter("poem.txt");
+		writer.write("En un lugar muy lejano de la galaxia \nStar wars");
+		writer.append("\n(A poem by me");
+		writer.close();
+		System.out.println("ya");
+	}catch (IOException e) {
+		e.printStackTrace();
+	}
+	}
+}
